d9f9c630e2bf907a48e82a689e329c3e7b83fe65
Glitch (cookie-parallelogram) <none>
2017-11-30 09:49:04 +0000

    👊🚊 Updated with Glitch

--- server.js
+++ server.js
@@ -2,97 +2,22 @@
 // init project
 var express = require('express');
 var app = express();
-var crypto = require("crypto");
-var mongodb = require('mongodb');
-var got = require('got');
-var iSearcher = require('./image-searcher.js');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-//db connection
-var MongoClient = mongodb.MongoClient;
-var mUrl = process.env.COMPLETE; //for mongodb
-var colName = "termLog";
+var multer  = require('multer');
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage }) //Buffer storage (in memory)
 
-//middleware to update mongodb log
-app.use('/api/imagesearch/', function (request, response, next) {
-  if(request.path.slice(1) == ""){    
-    response.redirect("/");
-    next();
-  }
-  else{
-    //isolate the query terms
-    var spaceReplace = new RegExp(/(%20)+/,'g');
-    var toSearch = request.path.slice(1).replace(spaceReplace, " ");
-    var whenSearch = new Date();
-    //build the log entry
-    var qToPush = {
-      term: toSearch,
-      when: whenSearch
-    };  
-    //mongodb
-    MongoClient.connect(mUrl, function (err, db) {
-      if (err) {
-        console.log('Unable to connect to the mongoDB server. Error:', err);
-        next(); //exit route
-      }
-      else {
-        db.collection(colName).updateOne({
-          searchType: 'image'
-        },{//update
-          $push: { queries: qToPush}
-        },{//options        
-          upsert: true
-        }); //updateOne
-        db.close(); //sync
-        next(); //sync    
-      }
-    });//mongo callback
-  }//empty check 'else'
+app.post('/uploader/', upload.single('gift'), function(request, response, next){
+  // req.file `gift` file 
+  // req.body text fields
+  console.log(request.file.size);
+  response.json(jfyer(request.file.size));
+    
 });
 
-//middleware to perform Google API call and Express response
-app.use('/api/imagesearch/', iSearcher({
-  //option1: '1', option2: '2' //arbitrary, unused options parameters
-}));
-
-//call mongodb, serve the log as json response
-app.use('/api/latest/imagesearch/', function(request, response, next){
-  
-  var ObjectID = require('mongodb').ObjectID;
-  var logId = new ObjectID("5a1fb80dcaa1f61c635becc0");
-  
-  MongoClient.connect(mUrl, function (err, db) {      
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    }
-    else {
-      //var docOut = 
-      db.collection(colName).findOne({
-        '_id': logId //find one, index to log
-      },{//options
-        fields:{          '_id': 0        } //hide the mongo _id
-      },
-      //callback on db response
-      function (err,doc){
-        if (err) {
-          console.log('findOne() query error: ', err);
-        }
-        //doc found by query
-        else if ( doc !== null ) {          
-          response.send(doc);//.queries);          
-        db.close();
-        }
-        //go to the next middleware
-        else {
-          console.log('log doc not found _id: ['+ logId +']');
-          next();
-        }
-      });//findOne callback
-    }  
-  });//mongo callback
-});
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -101,8 +26,8 @@ app.get("/", function (request, response) {
 
 
 //simplify the response calls
-function jfyer (longOne, shortOne){  
-  var jOut = { "term": longOne, "when": shortOne};  
+function jfyer (longOne){  
+  var jOut = { "size": longOne};  
   var stringOut = JSON.stringify(jOut);   
   //return stringOut;
   return jOut;
